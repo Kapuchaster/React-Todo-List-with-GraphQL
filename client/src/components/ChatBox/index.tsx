9f92b9331f40b0a91b426d2cebcfb2fc7ea4ca02
Michał Kapuściński <kapuchaster@gmail.com>
2022-12-16 23:01:50 +0100

    Format message time

--- client/src/components/ChatBox/index.tsx
+++ client/src/components/ChatBox/index.tsx
@@ -1,3 +1,4 @@
+import getMessageFormatTime from "../../services/time";
 import { Message } from "../../__generated__/operations-types";
 import ScrollBox from "../ScrollBox";
 
@@ -10,12 +11,13 @@ const MessageTile = ({
   isMine,
 }: Message & { isMine: boolean }) => {
   const isRight = isMine ? "chatBox__message--right" : "chatBox__message--left";
+  const formattedTime = getMessageFormatTime(timestamp);
 
   return (
     <div className={`chatBox__message--container ${isRight}`}>
       <div className="chatBox__message--info">
         <div>{author.name}</div>
-        <div>{timestamp}</div>
+        <div>{formattedTime}</div>
       </div>
       <div className="chatBox__message">{text}</div>
     </div>
---
+++ client/src/services/time.ts
@@ -0,0 +1,14 @@
+/** Takes timestamp and formates it to the hh:mm:ss format */
+const getMessageFormatTime = (timestamp: string) => {
+  const date = new Date(Number(timestamp));
+  const hours = date.getHours().toString();
+  let minutes = date.getMinutes().toString();
+  let seconds = date.getHours().toString();
+
+  if (minutes < "10") minutes = "0" + minutes;
+  if (seconds < "10") seconds = "0" + seconds;
+
+  return `${hours}:${minutes}:${seconds}`;
+};
+
+export default getMessageFormatTime;
