d27bf13d69e6db301fbd102f519acd83fcc19714
Michał Kapuściński <kapuchaster@gmail.com>
2022-12-31 13:06:00 +0100

    Refactor Aside component

--- client/src/components/layouts/AsidePanel/index.tsx
+++ client/src/components/layouts/AsidePanel/index.tsx
@@ -1,4 +1,4 @@
-import { Box, Button } from "@chakra-ui/react";
+import { Box } from "@chakra-ui/react";
 import { ReactElement } from "react";
 
 import "./style.css";
@@ -20,20 +20,23 @@ const AsidePanel = ({
 
   return (
     <Box
+      as="aside"
       position="absolute"
+      h="100vh"
       top="0"
       zIndex="2"
-      left={side === "left" ? 0 : "auto"}
-      right={side === "left" ? "auto" : 0}
       paddingLeft={side === "left" ? "0" : "30px"}
       paddingRight={side === "left" ? "30px" : "0"}
-      overflow="hidden"
+      className={`aside__${side}--${isActive}`}
     >
-      <aside className={`aside aside__${side}--${isActive}`}>
-        <Box backgroundColor={backgroundColor} height="100%">
-          {children}
-        </Box>
-      </aside>
+      <Box
+        boxShadow='2xl'
+        bg={backgroundColor}
+        height="100%"
+        width="20rem"
+      >
+        {children}
+      </Box>
     </Box>
   );
 };
