88e720fb749166f4417bfcf752d7c200cabc9610
Michał Kapuściński <kapuchaster@gmail.com>
2023-01-26 22:13:14 +0100

    Fix: repair time formater

--- client/src/services/time.ts
+++ client/src/services/time.ts
@@ -1,9 +1,11 @@
 /** Takes timestamp and formates it to the hh:mm:ss format */
 const getMessageFormatTime = (timestamp: string) => {
+  console.log(timestamp);
+  
   const date = new Date(Number(timestamp));
   const hours = date.getHours().toString();
   let minutes = date.getMinutes().toString();
-  let seconds = date.getHours().toString();
+  let seconds = date.getSeconds().toString();
 
   if (minutes < "10") minutes = "0" + minutes;
   if (seconds < "10") seconds = "0" + seconds;
